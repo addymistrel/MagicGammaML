c697d2f9bb7fccb847c3eaaf4f5e54a43933bb5e
addymistrel <102992848+addymistrel@users.noreply.github.com>
2023-12-24 15:41:40 +0530

    neural added

--- server/ml_apis/predict.py
+++ server/ml_apis/predict.py
@@ -1,6 +1,6 @@
 import joblib
 import os
-# from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 
 class Predict:
     def __init__(self,inp):
@@ -11,6 +11,20 @@ class Predict:
         modelPath = "modelTraining/randomForest.joblib"
         randomForest = joblib.load(modelPath)
         y_pred = randomForest.predict([self.input])
+        out = ""
+        print("output class",y_pred)
+        if y_pred[0]==0:
+            out="Hadron"
+        else:
+            out="Gamma"
+        return out
+    
+    def predictUsingNeural(self):
+        print("Current Working Directory:", os.getcwd())
+        modelPath = "modelTraining/neuralModel.h5"
+        randomForest = load_model(modelPath)
+        y_pred = randomForest.predict([self.input])
+        y_pred = (y_pred>0.5).astype(int).reshape(-1,)
         out = ""
         print("output class",y_pred)
         if y_pred[0]==0:
