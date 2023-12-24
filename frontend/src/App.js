651f3ef17aea2da01d1abf788c93b75f9a14a1e4
addymistrel <102992848+addymistrel@users.noreply.github.com>
2023-12-24 10:54:15 +0530

    input ui ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./form.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input
+          id="input-1"
+          type="number"
+          placeholder="Example - 28.796"
+          step="0.001"
+          required
+          autofocus
+        />
+        <label for="input-1">
+          <span class="label-text">fLength</span>
+          <span class="nav-dot"></span>
+          <div class="signup-button-trigger">Make Prediction</div>
+        </label>
+        <input
+          id="input-2"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-2">
+          <span class="label-text">fWidth</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-3"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-3">
+          <span class="label-text">fSize</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-4"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-4">
+          <span class="label-text">fConc</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-5"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-5">
+          <span class="label-text">fConc1</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-6"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-6">
+          <span class="label-text">fAsym</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-7"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-7">
+          <span class="label-text">fM3Long</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-8"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-8">
+          <span class="label-text">fM3Trans</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-9"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-9">
+          <span class="label-text">fAlpha</span>
+          <span class="nav-dot"></span>
+        </label>
+        <input
+          id="input-10"
+          type="number"
+          step="0.001"
+          placeholder="Example - 28.796"
+          required
+        />
+        <label for="input-10">
+          <span class="label-text">fDist</span>
+          <span class="nav-dot"></span>
+        </label>
+        <button type="submit">Predict Now</button>
+        <p class="tip">Press Tab</p>
+        <div class="signup-button">Make Prediction</div>
+      </form>
+    </>
   );
 }
 
